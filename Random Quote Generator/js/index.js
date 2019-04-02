ec0a267e8f836aae70d34af79d8af21fb8dc041a
Kshitijaa Jaglan <29124655+KshitijaaJ@users.noreply.github.com>
2019-04-03 00:14:36 +0530

    Update index.js

--- Random Quote Generator/js/index.js
+++ Random Quote Generator/js/index.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
   $("#nextQuote").on("click", function(){ // to be executed when the user clicks the button with id as #nextQuote
     var quoteIndex = Math.floor(Math.random() * 80); //gets a random number to be used as index for the random quote
       $.getJSON("https://gist.githubusercontent.com/KshitijaaJ/ce98c45e5885ff9c9c7876ee0b6a7b3f/raw/21964d0c26407a1f7f8f209ec6a93dda34bf2ff2/Quotes.json", function(json){ //gets the json file as the variable json
-        $(".quote-text").html(json[quoteIndex].quote);
-        $(".author").html(json[quoteIndex].author);
+        $(".quote-text").html(json[quoteIndex].quoteText);
+        $(".author").html(json[quoteIndex].quoteAuthor);
       });
   }); 
       $(".tweet-quote").on("click", function(){
